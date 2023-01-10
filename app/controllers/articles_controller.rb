7d82c02c503b95573a240491e18969dfdfaa7ab3
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-10 18:42:02 +0900

    createアクションとプラーベートメソッド記述

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,13 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+
+  def create
+    Article.create(article_params)
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :image, :content)
+  end
 end
