11f50db6cd0ea3ae8a0d4cbc0a8f73c763ca1434
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-11 15:25:32 +0900

    詳細画面のbtnビュー編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
-  resources :articles, only: [:index, :new ,:create, :show]
+  resources :articles, only: [:index, :new ,:create, :show, :edit, :destroy]
 end
