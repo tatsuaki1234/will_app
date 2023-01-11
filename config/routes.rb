4977fa1e178127428938055c7221ffe63706a075
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-11 19:05:18 +0900

    マイページの記述

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'articles#index'
   resources :articles, only: [:index, :new ,:create, :show, :edit, :update, :destroy]
+  resources :users, only: :show
 end
