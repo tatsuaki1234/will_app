1ab2439a4ed1fc798250ee2c734b7cd011732224
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-11 15:44:29 +0900

    :updateの後に,を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
-  resources :articles, only: [:index, :new ,:create, :show, :edit, :update :destroy]
+  resources :articles, only: [:index, :new ,:create, :show, :edit, :update, :destroy]
 end
