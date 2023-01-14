1909c09254e7642c247f785a2805989e884a1d81
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-14 18:54:04 +0900

    シークレットキー入力後wills#indexに遷移

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   root to: 'articles#index'
   resources :articles, only: [:index, :new ,:create, :show, :edit, :update, :destroy]
   resources :users, only: :show
-  resources :wills, only: :index
+  resources :wills, only: [:index]
   # get '/will_app_top_page/:id', to: 'patients#show'
   # HTTPメソッド 'URIパターン', to: 'コントローラー名#アクション名'
   # get 'posts', to: 'posts#index'
   get 'will_app_top_page', to: 'wills#will_app_top_page'
   get 'will_app_second_page', to: 'wills#will_app_second_page'
-  post 'will_app_third_page', to: 'wills#will_app_third_page'
-  get 'will_app_4th_page', to: 'wills#will_app_4th_page'
+  post 'wills', to: 'wills#index'
+  # get 'will_app_4th_page', to: 'wills#will_app_4th_page'
 end
 
 
