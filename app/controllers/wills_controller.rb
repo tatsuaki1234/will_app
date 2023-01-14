8cc406eae7f22e66284629d311e6a0d3af5322b1
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-14 17:54:30 +0900

    未ログイン時はログイン画面へ遷移する

--- app/controllers/wills_controller.rb
+++ app/controllers/wills_controller.rb
@@ -1,14 +1,10 @@
 class WillsController < ApplicationController
+  before_action :authenticate_user!, only: [ :will_app_second_page]
 
   def will_app_top_page
-    # @title = "本の題名"
+  end
 
-    # # 指定したファイルの中身を文字列で返す
-    # # layoutオプションの値をfalseにしておくと、レイアウトが適用されていない状態で取得できる
-    # template = render_to_string('wills/will_app_top_page', layout: false)
-
-    # # HTMLファイルを生成
-    # send_data(template, filename: "will_app_top_page.html")
+  def will_app_second_page
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # HTTPメソッド 'URIパターン', to: 'コントローラー名#アクション名'
   # get 'posts', to: 'posts#index'
   get 'will_app_top_page', to: 'wills#will_app_top_page'
+  get 'will_app_second_page', to: 'wills#will_app_second_page'
 end
 
 
