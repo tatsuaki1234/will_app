15635480361c227554071e93330d296feb0f8520
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-15 13:48:21 +0900

    遺書コントローラ[不要なコメントアウト削除]

--- app/controllers/wills_controller.rb
+++ app/controllers/wills_controller.rb
@@ -13,30 +13,17 @@ class WillsController < ApplicationController
   end
 
   def will_app_reader_page_2
-    # @wills = current_user.wills
   end
 
   def will_app_reader_page_3
-    # @wills = current_user.wills
   end
 
   def will_app_reader_page_4
-    # @wills = current_user.wills
     @will = Will.find(params[:will_id])
   end
 
 
   def index
-    # @wills = Will.all
-    # @will = Will.find(13)
-    # @will = Will.all
-    # @articles = Article.all
-    # @wills = current_user.objectives.all.order(created_at: :desc)
-    # @wills = current_user.Wills.all.order(created_at: :desc)
-
-    # @wills = current_user.wills
-    
-    # @wills = Will.find(current_user.wills)
   end
 
   def new
@@ -44,9 +31,6 @@ class WillsController < ApplicationController
   end
 
   def create
-    # Will.create(will_params)
-    # @will = will.new(will_params)
-    # if @will.save
     if Will.create(will_params)
       redirect_to wills_path
     else
