a0894177dd25d45477e07656b0a1b48299033775
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-23 16:19:55 +0900

    記事新規投稿のバリテーションを記述

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,6 +4,10 @@ class Article < ApplicationRecord
 
   belongs_to :user
 
+  #空の投稿を保存できないようにする
+  validates :title, presence: true
+  validates :content, presence: true
+
   def self.search(search)
     if search != ""
       Article.where('title LIKE(?)', "%#{search}%")
