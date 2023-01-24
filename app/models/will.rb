2bcfeaa6fd337e2a7317858f6c10073159b2c96f
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-24 14:32:39 +0900

    遺書にバリテーションを設定

--- app/models/will.rb
+++ app/models/will.rb
@@ -3,4 +3,8 @@ class Will < ApplicationRecord
   # validates :content, presence: true
 
   belongs_to :user
+
+  #空の投稿を保存できないようにする
+  validates :will_address, presence: true
+  validates :content, presence: true
 end
