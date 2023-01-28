e3f8a5c2db0117b88d4115345c25523ecc9f5843
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-28 09:43:36 +0900

    rspecをgemファイルに追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,6 @@ class Article < ApplicationRecord
   belongs_to :user
 
   #空の投稿を保存できないようにする
-  # validates :title, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は漢字かひらがなかカタカナで入力して下さい。' }
   validates :title, presence: true
   validates :content, presence: true
   
