a07e7be5b5b35dd12fd417c5f5d263df1cc48de6
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-28 10:03:39 +0900

    nameが空のエラーコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # before do
+  #   @user = FactoryBot.build(:user)
+  # end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
+      # it 'nameとnicknameとemail、passwordとencrypted_password、day_of_birth、secret_keyが存在すれば登録できる' do
+      #   expect(@user).to be_valid
+      # end
+    end
+
+    context '新規登録できないとき' do
+      it 'nameが空では登録できない' do
+        user = User.new(name: "", nickname: 'testニックネーム', email: 'test@example', password: '000000', password_confirmation: '000000', day_of_birth: "1996/12/17", secret_key: "123test" )
+        user.valid?
+        expect(user.errors.full_messages).to include("名前(本名)を入力してください")
+        # binding.pry
+
+      end
+      it 'emailが空では登録できない' do
+        # emailが空では登録できないテストコードを記述します
+      end
+    end
+  end
 end
