35b395068f4725fe17eab970e6c95299f76bcf87
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-28 11:20:40 +0900

    パスワードは英数字込みで登録するバリテーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :nickname, presence: true
   validates :email, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze, message: 'は英字と数字の組み合わせで入力して下さい。' }
   validates :encrypted_password, presence: true
   validates :day_of_birth, presence: true
   validates :secret_key, presence: true
