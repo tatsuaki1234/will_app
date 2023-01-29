69a359521c426bbb38ee4ad875b8396ff9a7cf12
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-29 12:37:27 +0900

    userが紐づいていないと投稿できないエラーコード記述

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -21,14 +21,13 @@ RSpec.describe Article, type: :model do
         it 'contentが空では投稿できない' do
           @article.content = ''
           @article.valid?
-          expect(@item.errors.full_messages).to include("記事内容を入力してください")
+          expect(@article.errors.full_messages).to include("記事内容を入力してください")
         end          
         
         it 'userが紐付いていなければ投稿できない' do
-          # @item.user = nil
-          # @item.valid?
-          binding.pry
-          # expect(@item.errors.full_messages).to include("ユーザーを入力してください")
+          @article.user = nil
+          @article.valid?
+          expect(@article.errors.full_messages).to include("ユーザーを入力してください")
         end          
       end
     end
