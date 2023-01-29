7ed4f7f18c3949e959892517ab5410f2e2951e11
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-29 12:33:42 +0900

    titleが空では投稿できないエラーコード記述

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @article = FactoryBot.build(:article)
+  end
+
+  describe '記事新規投稿' do
+    context '記事新規投稿できるとき' do
+      it 'title、contentが存在すれば投稿できる' do
+        expect(@article).to be_valid
+      end
+
+      context '記事投稿できないとき' do
+        it 'titleが空では投稿できない' do
+          @article.title = ''
+          @article.valid?
+          expect(@article.errors.full_messages).to include("タイトルを入力してください")
+        end
+
+        it 'contentが空では投稿できない' do
+          @article.content = ''
+          @item.valid?
+          binding.pry
+          # expect(@item.errors.full_messages).to include("商品説明分を入力してください")
+        end          
+        
+        it 'userが紐付いていなければ投稿できない' do
+          # @item.user = nil
+          # @item.valid?
+
+          # expect(@item.errors.full_messages).to include("ユーザーを入力してください")
+        end          
+      end
+    end
+  end
 end
