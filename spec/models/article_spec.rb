03019986300e2619a9fa65ac2c1cd0bd53a53a6d
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-29 12:34:37 +0900

    contentが空では投稿できないエラーコード記述

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -20,15 +20,14 @@ RSpec.describe Article, type: :model do
 
         it 'contentが空では投稿できない' do
           @article.content = ''
-          @item.valid?
-          binding.pry
-          # expect(@item.errors.full_messages).to include("商品説明分を入力してください")
+          @article.valid?
+          expect(@item.errors.full_messages).to include("記事内容を入力してください")
         end          
         
         it 'userが紐付いていなければ投稿できない' do
           # @item.user = nil
           # @item.valid?
-
+          binding.pry
           # expect(@item.errors.full_messages).to include("ユーザーを入力してください")
         end          
       end
