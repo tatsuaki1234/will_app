d64b0256801028c798dfa2e55a05b3b6a021ca90
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-30 12:34:55 +0900

    willモデルのテストファイル生成

---
+++ spec/factories/wills.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :will do
+    
+  end
+end
---
+++ spec/models/will_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Will, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
