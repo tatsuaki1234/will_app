c619886ad3fd98c8f9f48ac6db8830b29af5abd6
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-01-30 12:38:24 +0900

    willテストコード用beforeaction記述

--- spec/models/will_spec.rb
+++ spec/models/will_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Will, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @will = FactoryBot.build(:will)
+  end
 end
