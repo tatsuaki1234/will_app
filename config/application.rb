07bfa8c355b6123ec416ad865528a35de37e5202
tatsuaki1234 <76762026+tatsuaki1234@users.noreply.github.com>
2023-02-12 11:31:41 +0900

    日本時刻の設定を追記

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module WillApp
     # 日本語の言語設定
     config.i18n.default_locale = :ja
 
+    # 日本の時刻設定
+    config.time_zone = 'Tokyo'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
